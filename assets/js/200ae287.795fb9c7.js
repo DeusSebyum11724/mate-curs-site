7bbc0e93d1043c905326f0008f9ce32d9b976cea
Sebastian-Andrei Brisan <asbri@Sebastian-Andreis-MacBook-Air.local>
2025-04-16 15:55:00 +0300

    Deploy website - based on 1ecbc9f48b886d898e3f4f2b468208a22752fd17

---
+++ assets/js/200ae287.795fb9c7.js
@@ -0,0 +1,56 @@
+"use strict";(self.webpackChunkalf_site=self.webpackChunkalf_site||[]).push([[6998],{2950:(e,t,r)=>{r.r(t),r.d(t,{assets:()=>a,contentTitle:()=>d,default:()=>o,frontMatter:()=>c,metadata:()=>i,toc:()=>x});const i=JSON.parse('{"id":"C6.1 \u0219i 6.2 \u2013 Ridicarea la putere \u0219i radicali","title":"C6.1 \u0219i 6.2 \u2013 Ridicarea la putere \u0219i radicali","description":"---","source":"@site/docs/C6.1 \u0219i 6.2 \u2013 Ridicarea la putere \u0219i radicali.md","sourceDirName":".","slug":"/C6.1 \u0219i 6.2 \u2013 Ridicarea la putere \u0219i radicali","permalink":"/mate-curs-site/docs/C6.1 \u0219i 6.2 \u2013 Ridicarea la putere \u0219i radicali","draft":false,"unlisted":false,"editUrl":"https://github.com/DeusSebyum11724/mate-curs-site/edit/main/docs/C6.1 \u0219i 6.2 \u2013 Ridicarea la putere \u0219i radicali.md","tags":[],"version":"current","frontMatter":{},"sidebar":"tutorialSidebar","previous":{"title":"calculus","permalink":"/mate-curs-site/docs/calculus"},"next":{"title":"probability","permalink":"/mate-curs-site/docs/probability"}}');var n=r(4848),s=r(8453),l=r(8584);const c={},d=void 0,a={},x=[{value:"No\u021biuni:",id:"no\u021biuni",level:3},{value:"Regula descompunerii \xeen factori primi:",id:"regula-descompunerii-\xeen-factori-primi",level:3},{value:"Propriet\u0103\u021bi importante ale radicalilor:",id:"propriet\u0103\u021bi-importante-ale-radicalilor",level:3},{value:"Tabel exemplificativ:",id:"tabel-exemplificativ",level:3}];function h(e){const t={code:"code",h3:"h3",hr:"hr",p:"p",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",...(0,s.R)(),...e.components};return(0,n.jsxs)(n.Fragment,{children:[(0,n.jsx)(t.hr,{}),"\n",(0,n.jsx)(t.h3,{id:"no\u021biuni",children:"No\u021biuni:"}),"\n",(0,n.jsx)(l.A,{children:String.raw`
+\begin{aligned}
+& 6^{2}=(-6)^{2}=36,\quad\text{unde:}\quad
+\begin{cases}
+6 \cdot 6 = 36 \\[3pt]
+(-6) \cdot (-6) = 36
+\end{cases}\\[6pt]
+& a^{2}=(-a)^{2}\\[6pt]
+& (\sqrt{a})^{2}=\sqrt{a^{2}}=a \quad \text{(20. Noțiune)}
+\end{aligned}
+`}),"\n",(0,n.jsx)(t.hr,{}),"\n",(0,n.jsx)(t.h3,{id:"regula-descompunerii-\xeen-factori-primi",children:"Regula descompunerii \xeen factori primi:"}),"\n",(0,n.jsx)(t.p,{children:"Numere prime:"}),"\n",(0,n.jsx)(l.A,{children:String.raw`2,\;3,\;5,\;7,\;11,\;13,\;17,\;19,\;23,\;29,\;31,\;37,\;\dots`}),"\n",(0,n.jsx)(l.A,{children:String.raw`
+\begin{aligned}
+& 9^{2}=81=3^{4}=\left(3^{2}\right)^{2}\\[5pt]
+& \sqrt{81}=\sqrt{3^{4}}=\sqrt{\left(3^{2}\right)^{2}}=3^{2}=9
+\end{aligned}
+`}),"\n",(0,n.jsx)(t.p,{children:"Descompunerea lui 81 \xeen factori primi:"}),"\n",(0,n.jsx)(l.A,{children:String.raw`
+\left.\begin{array}{r|r}
+81 & 3 \\
+27 & 3 \\
+9 & 3 \\
+3 & 3 \\
+1 &  
+\end{array}\right\}\quad\text{4 pași (împărțiri succesive la 3)}
+`}),"\n",(0,n.jsx)(t.hr,{}),"\n",(0,n.jsx)(l.A,{children:String.raw`
+\begin{aligned}
+\sqrt{512}&=\sqrt{2^{9}}=\sqrt{2 \cdot 2^{8}}\\[3pt]
+&=\sqrt{2}\cdot\sqrt{2^{8}}=\sqrt{2}\cdot\sqrt{\left(2^{4}\right)^{2}}\\[3pt]
+&=\sqrt{2}\cdot 2^{4}=16\sqrt{2}
+\end{aligned}
+`}),"\n",(0,n.jsx)(t.p,{children:"Descompunerea lui 512 \xeen factori primi:"}),"\n",(0,n.jsx)(l.A,{children:String.raw`
+\left.\begin{array}{r|r}
+512 & 2 \\
+256 & 2 \\
+128 & 2 \\
+64 & 2 \\
+32 & 2 \\
+16 & 2 \\
+8 & 2 \\
+4 & 2 \\
+2 & 2 \\
+1 & 
+\end{array}\right\}\quad\text{9 pași (împărțiri succesive la 2)}
+`}),"\n",(0,n.jsx)(t.hr,{}),"\n",(0,n.jsx)(l.A,{children:String.raw`
+\begin{aligned}
+\sqrt{384}&=\sqrt{2^{7}\cdot 3}=\sqrt{2^{7}}\cdot\sqrt{3}\\[3pt]
+&=\sqrt{2\cdot 2^{6}}\cdot\sqrt{3}\\[3pt]
+&=\sqrt{2}\cdot\sqrt{2^{6}}\cdot\sqrt{3}\\[3pt]
+&=\sqrt{(2^{3})^{2}}\cdot\sqrt{2\cdot 3}\\[3pt]
+&=2^{3}\cdot\sqrt{6}=8\sqrt{6}
+\end{aligned}
+`}),"\n",(0,n.jsx)(t.hr,{}),"\n",(0,n.jsx)(t.h3,{id:"propriet\u0103\u021bi-importante-ale-radicalilor",children:"Propriet\u0103\u021bi importante ale radicalilor:"}),"\n",(0,n.jsx)(l.A,{children:String.raw`\sqrt{a}\cdot\sqrt{b}=\sqrt{a\cdot b}`}),"\n",(0,n.jsx)("table",{children:(0,n.jsxs)("tr",{children:[(0,n.jsx)("td",{children:(0,n.jsx)("strong",{children:"Exemplu:"})}),(0,n.jsx)("td",{children:(0,n.jsx)(l.A,{children:String.raw`\sqrt{5}\cdot\sqrt{7}=\sqrt{35}`})})]})}),"\n",(0,n.jsx)(l.A,{children:String.raw`\frac{\sqrt{a}}{\sqrt{b}}=\sqrt{\frac{a}{b}}`}),"\n",(0,n.jsx)("table",{children:(0,n.jsxs)("tr",{children:[(0,n.jsx)("td",{children:(0,n.jsx)("strong",{children:"Exemplu:"})}),(0,n.jsx)("td",{children:(0,n.jsx)(l.A,{children:String.raw`\frac{\sqrt{32}}{\sqrt{2}}=\sqrt{\frac{32}{2}}=\sqrt{16}=4`})})]})}),"\n",(0,n.jsx)(l.A,{children:String.raw`\sqrt{a}+\sqrt{a}=2\cdot\sqrt{a}`}),"\n",(0,n.jsx)("table",{children:(0,n.jsxs)("tr",{children:[(0,n.jsx)("td",{children:(0,n.jsx)("strong",{children:"Exemplu:"})}),(0,n.jsx)("td",{children:(0,n.jsx)(l.A,{children:String.raw`\sqrt{5}+\sqrt{5}=2\sqrt{5}`})})]})}),"\n",(0,n.jsx)(l.A,{children:String.raw`2\cdot\sqrt{a}-\sqrt{a}=\sqrt{a}`}),"\n",(0,n.jsx)("table",{children:(0,n.jsxs)("tr",{children:[(0,n.jsx)("td",{children:(0,n.jsx)("strong",{children:"Exemplu:"})}),(0,n.jsx)("td",{children:(0,n.jsx)(l.A,{children:String.raw`2\sqrt{3}-\sqrt{3}=\sqrt{3}`})})]})}),"\n",(0,n.jsx)(t.hr,{}),"\n",(0,n.jsx)(t.h3,{id:"tabel-exemplificativ",children:"Tabel exemplificativ:"}),"\n",(0,n.jsxs)(t.table,{children:[(0,n.jsx)(t.thead,{children:(0,n.jsxs)(t.tr,{children:[(0,n.jsx)(t.th,{children:(0,n.jsx)(t.code,{className:"language-math math-inline",children:"a"})}),(0,n.jsx)(t.th,{style:{textAlign:"center"},children:"-25"}),(0,n.jsx)(t.th,{style:{textAlign:"center"},children:"25"}),(0,n.jsx)(t.th,{style:{textAlign:"center"},children:"1"}),(0,n.jsx)(t.th,{style:{textAlign:"center"},children:"-4"}),(0,n.jsx)(t.th,{style:{textAlign:"center"},children:"900"}),(0,n.jsx)(t.th,{style:{textAlign:"center"},children:"0,16"}),(0,n.jsx)(t.th,{style:{textAlign:"center"},children:"6"})]})}),(0,n.jsxs)(t.tbody,{children:[(0,n.jsxs)(t.tr,{children:[(0,n.jsx)(t.td,{children:(0,n.jsx)(l.A,{children:String.raw`\sqrt{a}`})}),(0,n.jsx)(t.td,{style:{textAlign:"center"},children:"\u2013"}),(0,n.jsx)(t.td,{style:{textAlign:"center"},children:"5"}),(0,n.jsx)(t.td,{style:{textAlign:"center"},children:"1"}),(0,n.jsx)(t.td,{style:{textAlign:"center"},children:"\u2013"}),(0,n.jsx)(t.td,{style:{textAlign:"center"},children:"30"}),(0,n.jsx)(t.td,{style:{textAlign:"center"},children:"0,4"}),(0,n.jsx)(t.td,{style:{textAlign:"center"},children:(0,n.jsx)(l.A,{children:String.raw`\sqrt{6}`})})]}),(0,n.jsxs)(t.tr,{children:[(0,n.jsx)(t.td,{children:(0,n.jsx)(l.A,{children:String.raw`a^{2}`})}),(0,n.jsx)(t.td,{style:{textAlign:"center"},children:"625"}),(0,n.jsx)(t.td,{style:{textAlign:"center"},children:"625"}),(0,n.jsx)(t.td,{style:{textAlign:"center"},children:"1"}),(0,n.jsx)(t.td,{style:{textAlign:"center"},children:"16"}),(0,n.jsx)(t.td,{style:{textAlign:"center"},children:"810000"}),(0,n.jsx)(t.td,{style:{textAlign:"center"},children:"0,0256"}),(0,n.jsx)(t.td,{style:{textAlign:"center"},children:"36"})]})]})]}),"\n",(0,n.jsx)(t.p,{children:"Explica\u021bii suplimentare pentru tabel:"}),"\n",(0,n.jsxs)(t.p,{children:["1. Pentru ",(0,n.jsx)(t.code,{className:"language-math math-inline",children:"a=-4"}),":"]}),"\n",(0,n.jsx)(l.A,{children:String.raw`a^{2}=(-4)^{2}=16`}),"\n",(0,n.jsxs)(t.p,{children:["2. Pentru ",(0,n.jsx)(t.code,{className:"language-math math-inline",children:"a=0,16"}),":"]}),"\n",(0,n.jsx)(l.A,{children:String.raw`
+\begin{aligned}
+a&=(0,4)^{2}=0,4\cdot 0,4=0,16\\[3pt]
+a^{2}&=0,16\cdot 0,16=0,0256
+\end{aligned}
+`}),"\n",(0,n.jsx)(t.hr,{})]})}function o(e={}){const{wrapper:t}={...(0,s.R)(),...e.components};return t?(0,n.jsx)(t,{...e,children:(0,n.jsx)(h,{...e})}):h(e)}},8453:(e,t,r)=>{r.d(t,{R:()=>l,x:()=>c});var i=r(6540);const n={},s=i.createContext(n);function l(e){const t=i.useContext(s);return i.useMemo((function(){return"function"==typeof e?e(t):{...t,...e}}),[t,e])}function c(e){let t;return t=e.disableParentContext?"function"==typeof e.components?e.components(n):e.components||n:l(e.components),i.createElement(s.Provider,{value:t},e.children)}},8584:(e,t,r)=>{r.d(t,{A:()=>s});r(6540);var i=r(2259),n=r(4848);function s(e){var t=e.children;return(0,n.jsx)(i.A,{block:!0,math:t})}}}]);
